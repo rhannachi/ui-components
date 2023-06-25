db15639af77923450d4f6ba8cc826f90c4e0488b
ramzi hannachi <rhannachi1991@gmail.com>
2023-06-25 13:03:08 +0200

    add coverage test

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    "@storybook/addon-coverage",
     {
       name: '@storybook/addon-styling',
       options: {
