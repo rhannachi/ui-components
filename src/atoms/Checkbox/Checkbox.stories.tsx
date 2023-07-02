ab315db1ef04d21b88326633eee6c85c95cc2f55
ramzi hannachi <rhannachi1991@gmail.com>
2023-07-02 13:36:12 +0200

    Feat - update Chackbox component

--- src/atoms/Checkbox/Checkbox.stories.tsx
+++ src/atoms/Checkbox/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react"
-import Checkbox from './Checkbox'
+import Checkbox, { COLORS_CHECKBOX, SIZES_CHECKBOX } from "./Checkbox"
 
 const meta: Meta<typeof Checkbox> = {
   component: Checkbox,
@@ -10,11 +10,21 @@ type Story = StoryObj<typeof Checkbox>;
 
 export const Default: Story = {
   argTypes: {
+    color: {
+      options: COLORS_CHECKBOX,
+      control: 'select'
+    },
+    size: {
+      options: SIZES_CHECKBOX,
+      control: 'select'
+    },
     checked: { control: 'boolean' },
     disabled: { control: 'boolean' },
     onChange: { action: 'onChange' }
   },
   args: {
+    color: 'cyan',
+    size: 'base',
     checked: true,
     label: 'checkbox',
     disabled: false,
--- src/atoms/Checkbox/Checkbox.tsx
+++ src/atoms/Checkbox/Checkbox.tsx
@@ -1,13 +1,50 @@
 import React, { useEffect, useState } from "react"
 import clsx from "clsx"
 
+/* SIZES */
+export const SIZES_CHECKBOX = ['sm', 'base', 'lg'] as const
+type SizeCheckBoxType = typeof SIZES_CHECKBOX[number]
+
+const SIZE_BOX : Readonly<Record<SizeCheckBoxType, string>> = {
+  'sm' : 'w-4 h-4',
+  'base' : 'w-5 h-5',
+  'lg' : 'w-6 h-6'
+}
+
+const SIZE_SVG : Readonly<Record<SizeCheckBoxType, string>> = {
+  'sm' : 'w-2 h-2',
+  'base' : 'w-3 h-3',
+  'lg': 'w-4 h-4'
+}
+
+/* COLORS */
+export const COLORS_CHECKBOX = [
+  'cyan',
+  'red',
+] as const
+
+type ColorCheckBoxType = typeof COLORS_CHECKBOX[number]
+
+const COLORS_BOX : Readonly<Record<ColorCheckBoxType, string>> = {
+  'cyan' : 'border-cyan-400',
+  'red' : 'border-red-400'
+}
+
+const COLORS_SVG : Readonly<Record<ColorCheckBoxType, string>> = {
+  'cyan' : 'fill-cyan-400',
+  'red' : 'fill-red-400'
+}
+
+/* PROPS */
 type CheckboxProps = Partial<Pick<HTMLInputElement, 'disabled' | 'checked' >> & {
   label?: string
+  color?: ColorCheckBoxType
+  size?: SizeCheckBoxType
   onChange?: (checked: boolean) => void
   className?: string
 }
 
-const Checkbox = ({ checked = false, label, onChange, className, ...other }: CheckboxProps) => {
+const Checkbox = ({ checked = false, size = 'base', color = 'cyan', label, onChange, className, ...other }: CheckboxProps) => {
   const [myChecked, setMyChecked] = useState<boolean>(checked)
 
   useEffect( () => setMyChecked(checked), [checked] )
@@ -17,23 +54,21 @@ const Checkbox = ({ checked = false, label, onChange, className, ...other }: Che
     <div className={clsx(className, 'flex items-center')}>
         <input {...other}
                  type="checkbox"
-                 id="A3-yes"
                  onChange={() => setMyChecked(!myChecked)}
                  checked={myChecked}
-                 value="yes"
-                 className="opacity-0 absolute h-4 w-4" />
-          <div className="bg-white border rounded border-cyan-400 w-4 h-4 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-cyan-400">
-            <svg className="fill-current hidden w-3 h-3 text-cyan-400 pointer-events-none" version="1.1"
+                 className={clsx(SIZE_BOX[size],"opacity-0 absolute")} />
+          <div className={clsx(COLORS_BOX[color] , SIZE_BOX[size],"bg-white border-2 rounded flex flex-shrink-0 justify-center items-center")}>
+            <svg className={clsx(SIZE_SVG[size] , "fill-current hidden  pointer-events-none")} version="1.1"
                  viewBox="0 0 17 12" xmlns="http://www.w3.org/2000/svg">
               <g fill="none" fill-rule="evenodd">
-                <g transform="translate(-9 -11)" fill="#22d3ee" fill-rule="nonzero">
+                <g transform="translate(-9 -11)" className={clsx(COLORS_SVG[color])} fill-rule="nonzero">
                   <path
                     d="m25.576 11.414c0.56558 0.55188 0.56558 1.4439 0 1.9961l-9.404 9.176c-0.28213 0.27529-0.65247 0.41385-1.0228 0.41385-0.37034 0-0.74068-0.13855-1.0228-0.41385l-4.7019-4.588c-0.56584-0.55188-0.56584-1.4442 0-1.9961 0.56558-0.55214 1.4798-0.55214 2.0456 0l3.679 3.5899 8.3812-8.1779c0.56558-0.55214 1.4798-0.55214 2.0456 0z" />
                 </g>
               </g>
             </svg>
           </div>
-          <label htmlFor="A3-yes" className="text-sm font-medium text-gray-900">{label}</label>
+          <label className="ml-2 text-sm font-medium text-gray-900">{label}</label>
     </div>
   )
 }
