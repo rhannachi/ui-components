81a97e27b0dd14e2ede481cba66cd185444ddbf8
ramzi hannachi <rhannachi1991@gmail.com>
2023-07-02 13:46:56 +0200

    Feat - update Icon component

--- src/atoms/Icon/Icon.tsx
+++ src/atoms/Icon/Icon.tsx
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from 'react'
 import clsx from 'clsx'
 import { ReactSVG } from 'react-svg'
 
+/* SIZES */
 export const SIZES_ICON = ['xs', 'sm', 'base', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl'] as const
 type SizeIconType = typeof SIZES_ICON[number]
-const SIZES_ICON_CUSTOM = [
-  'w-3 h-3',
-  'w-4 h-4',
-  'w-5 h-5',
-  'w-6 h-6',
-  'w-7 h-7',
-  'w-8 h-8',
-  'w-9 h-9',
-  'w-10 h-10',
-  'w-11 h-11',
-  'w-12 h-12',
-  'w-14 h-14',
-  'w-16 h-16'
-] as const
-type SizeIconCustomType = typeof SIZES_ICON_CUSTOM[number]
+const SIZES : Readonly<Record<SizeIconType, string>> = {
+  'xs': 'w-3 h-3',
+  'sm': 'w-4 h-4',
+  'base': 'w-5 h-5',
+  'lg' : 'w-6 h-6',
+  'xl': 'w-7 h-7',
+  '2xl': 'w-8 h-8',
+  '3xl': 'w-9 h-9',
+  '4xl': 'w-10 h-10',
+  '5xl': 'w-11 h-11',
+  '6xl': 'w-12 h-12',
+  '7xl': 'w-14 h-14',
+  '8xl': 'w-16 h-16'
+}
 
+/* ICONS */
 // to be able to change the size of the Icon, add  viewBox="0 0 25 25" to svg component
 export const ICONS = [
   'star',
@@ -37,6 +38,7 @@ export const ICONS = [
 ] as const
 type IconType = typeof ICONS[number]
 
+/* COLORS */
 export const COLORS_ICON = [
   'fill-gray-200',
   'fill-gray-300',
@@ -48,22 +50,18 @@ export const COLORS_ICON = [
   'fill-black',
   'fill-white'
 ] as const
-export type ColorIconType = typeof COLORS_ICON[number]
+type ColorIconType = typeof COLORS_ICON[number]
 
+/* PROPS */
 type IconProps = {
   icon: IconType
-  size: SizeIconType
+  size?: SizeIconType
   color?: ColorIconType
   className?: string
 }
 
 const importIcon = async (icon: IconType): Promise<string | { src: string }> => {
   return (await import(`./svgs/${icon}.svg`)).default
-}
-
-const getSize = (size: SizeIconType): SizeIconCustomType => {
-  const index = SIZES_ICON.findIndex((item) => item === size)
-  return SIZES_ICON_CUSTOM[index]
 }
 
 export const Icon = ({ icon, size = 'base', color = 'fill-black', className = '' }: IconProps) => {
@@ -83,7 +81,7 @@ export const Icon = ({ icon, size = 'base', color = 'fill-black', className = ''
 
   return (
     <ReactSVG
-      className={clsx(color, getSize(size), className, 'inline')}
+      className={clsx(color, SIZES[size], className, 'inline')}
       wrapper="svg"
       // loading={() => <span>is loading ...</span>}
       src={iconSrc}
