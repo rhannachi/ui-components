a5d7bfbce2e48f308e41deaac6e8e84774dc23b9
Ramzi Hannachi <75175915+rhannachi@users.noreply.github.com>
2023-07-22 11:10:21 +0200

    Refacto - install and configure rollup (#11)

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Atoms
+import { Checkbox } from '@/atoms/Checkbox'
+import { Icon } from '@/atoms/Icon'
+// Molecules
+import { Task } from '@/molecules/Task'
+import { TaskSkeleton } from '@/molecules/TaskSkeleton'
+// Organisms
+import { Tasks } from '@/organisms/Tasks'
+// Templates
+import { TasksBox } from '@/templates/TaskBox'
+
+export { Checkbox, Icon, Task, TaskSkeleton, Tasks, TasksBox }
